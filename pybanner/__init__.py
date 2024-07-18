3dced23853d56e0b9f7c92c8bacf17ce1a578515
Ricardo Abuchaim <ricardoabuchaim@gmail.com>
2024-07-18 13:55:45 -0300

    pybanner v1.0.1

--- pybanner/__init__.py
+++ pybanner/__init__.py
@@ -1,2 +1,6 @@
-#!/usr/bin/env python3
-from pybanner.pybanner import banner5x6, banner6x6
+from pybanner.pybanner import MIDDOT
+from pybanner.pybanner import banner_clr4x6
+from pybanner.pybanner import banner_clr5x6, banner_clr5x8, banner_clr5x10, banner_5x7, banner_5x8
+from pybanner.pybanner import banner_clr6x6, banner_clr6x8, banner_clr6x10, banner_6x9, banner_6x10
+from pybanner.pybanner import banner_clb6x10
+from pybanner.pybanner import banner_clr7x8, banner_clr7x10
